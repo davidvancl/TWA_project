d88b7c9c0c4fa88ce715b4da3204881d26564d83
vancl <vancldada@gmail.com>
2021-12-29 00:37:49 +0100

    admin pagination

--- public/js/admin.blocek.js
+++ public/js/admin.blocek.js
@@ -1,3 +1,9 @@
-function upgradeUserRole(href){
+function action(href){
     document.location.href = href
+}
+
+function confirmAction(href){
+    if (confirm('Opravdu chceš smazat uživatele?')) {
+        document.location.href = href
+    }
 }
--- src/Controller/Action/AdminController.php
+++ src/Controller/Action/AdminController.php
@@ -11,20 +11,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="app_admin")
+     * @Route("/admin/{skip}", name="app_admin")
      */
-    public function admin(Connection $conn): Response
+    public function admin(int $skip = null, Connection $conn): Response
     {
+        $page = 10;
         $queryBuilder = $conn->createQueryBuilder();
         $queryBuilder
             ->select('id, email, name, surname, roles, gender')
-            ->from('user');
+            ->from('user')
+            ->setMaxResults($page);
+        if($skip) {
+            $queryBuilder->setFirstResult($skip * $page);
+        }
+
+        $total = $conn->createQueryBuilder()
+            ->select('COUNT(id) as count')
+            ->from('user')
+            ->execute()
+            ->fetch();
 
         $registered_users = $queryBuilder
             ->execute()
             ->fetchAll();
 
+        $odd_number = (intval($total['count']) % 2) == 1;
+
+        $pages =((intval($total['count']) / $page) < 1) ? [1] : range(1, (intval($total['count']) / $page) + (($odd_number != 0) ? 1 : 0));
+
         return $this->render('Main/admin.blocek.html.twig', [
+            'pages' => $pages,
             'registered_users' => $registered_users,
             'subtitle' => 'Admin'
         ]);
@@ -33,7 +49,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/role/{id}/{role}", name="role_changer")
      */
-    public function change_role (int $id, string $role, Connection $conn): RedirectResponse
+    public function change_role(int $id, string $role, Connection $conn): RedirectResponse
     {
         $allowed_roles = ["ROLE_ADMIN", "ROLE_USER"];
         if(!in_array("ROLE_ADMIN", $this->getUser()->getRoles())){
@@ -56,6 +72,27 @@ class AdminController extends AbstractController
             ->execute();
 
         $this->addFlash('success', "Role byla změněna.");
+        return $this->redirectToRoute('app_admin');
+    }
+
+    /**
+     * @Route("/user/delete/{id}", name="delete_user")
+     */
+    public function delete_user(int $id, Connection $conn): RedirectResponse
+    {
+        if (!in_array("ROLE_ADMIN", $this->getUser()->getRoles())){
+            $this->addFlash('error', $this->getParameter('message.permissions_required'));
+            return $this->redirectToRoute('app_admin');
+        }
+
+        $queryBuilder = $conn->createQueryBuilder();
+        $queryBuilder
+            ->delete('user')
+            ->where('id = :id_user')
+            ->setParameter("id_user", $id)
+            ->execute();
+
+        $this->addFlash('success', "Uživatel byl smazán.");
         return $this->redirectToRoute('app_admin');
     }
 }
