ecda6a8bdd95cd9bb75369e9855c9ba86659d0ec
vancl <vancldada@gmail.com>
2022-01-03 20:16:09 +0100

    fixes

---
+++ migrations/Version20220103160745.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220103160745 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE help (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(100) NOT NULL, section VARCHAR(100) NOT NULL, data VARCHAR(100) NOT NULL, UNIQUE INDEX UNIQ_8875CAC2D737AEF (section), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE help');
+    }
+}
